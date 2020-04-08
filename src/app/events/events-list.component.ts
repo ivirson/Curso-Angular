147e47336a36bfbb3b8068c9634fadf18705d837
Ivirson M. Daltro <ivirson.m.daltro@avanade.com>
2020-04-08 17:57:32 -0300

    módulo 2

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular events</h1>
+        <hr />
+        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <h3>{{thumbnail.someProperty}}</h3>
+        <button class="btn btn-primary" (click)="thumbnail.logFoo()">
+            Log me some foo!
+        </button>
+    </div>
+    `
+})
+
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '01/05/2020',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: 'assets/images/basic-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+}
